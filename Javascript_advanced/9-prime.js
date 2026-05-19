255083232c252712258b78875c66e9c3d1db44cf
SamJouini <samantha.u.h.jouini@gmail.com>
2026-05-19 13:32:45 +0200

    task 10

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,28 @@
+function countPrimeNumbers() {
+  let count = 0;
+
+  for (let i = 2; i <= 100; i++) {
+    let isPrime = true;
+
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) count++;
+  }
+
+  return count;
+}
+
+const start = performance.now();
+
+countPrimeNumbers();
+
+const end = performance.now();
+
+console.log(
+  `Execution time of printing countPrimeNumbers was ${end - start} milliseconds.`
+);
