728d92ba8823638594a31ab65cfafb85a117b384
SamJouini <samantha.u.h.jouini@gmail.com>
2026-05-19 12:04:32 +0200

    task 7

---
+++ Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,24 @@
+class studentHogwarts {
+  #privateScore = 0;
+  #name = null;
+
+  #changeScoreBy(points) {
+    this.#privateScore += points;
+  }
+
+  setName(newName) {
+    this.#name = newName;
+  }
+
+  rewardStudent() {
+    this.changeScoreBy(1);
+  }
+
+  penalizeStudent() {
+    this.changeScoreBy(-1);
+  }
+
+  getScore() {
+    return `${this.#name}: ${this.#privateScore}`;
+  }
+}
