11a8c8d37438c97383b2e2dca08bf4a40d415e9c
SamJouini <samantha.u.h.jouini@gmail.com>
2026-05-19 10:46:44 +0200

    javascript advanced - task 1

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,9 @@
+function welcome (firstName, lastName) {
+ const fullName = `${firstName} ${lastName}`;
+ 
+ function displayFullName() {
+    alert(`Welcome ${fullName}!`);
+  }
+
+  displayFullName();
+}
