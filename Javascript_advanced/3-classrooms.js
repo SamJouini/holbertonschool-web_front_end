60e61495456a3323886a4361f2ae2e4b035248ba
SamJouini <samantha.u.h.jouini@gmail.com>
2026-05-19 11:33:32 +0200

    task 4

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,18 @@
+function createClassRoom(numberOfStudents) {
+
+  function studentSeat(seat) {
+    return function () {
+      return seat;
+    };
+  }
+
+  let students = [];
+
+  for (let i = 0; i < numberOfStudents; i++) {
+    students.push(studentSeat(i + 1));
+  }
+
+  return students;
+}
+
+const classRoom = createClassRoom(10);
